53dadb6f560da4c5c7a2f3316e29cda9af3ef74b
Joaquin <joaquinleonquero00@gmail.com>
2023-02-06 21:39:47 -0600

    Creación de Dark-Mode al sitio y correción de algunos estilos

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,33 @@
 document.addEventListener('DOMContentLoaded', function(){
     eventlisteners();
+
+    darkMode();
 });
+
+function darkMode(){
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');   
+    }
+
+    prefiereDarkMode.addEventListener('change', function(){
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');   
+        }
+    });
+    
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode');
+    });
+}
+
 
 function eventlisteners(){
     const mobileMenu = document.querySelector('.mobile-menu');
