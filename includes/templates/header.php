3ca7b26b30ca5785da330ba295851a0cb4a39530
joaquin leonquero <joaquinleonquero00@gmail.com>
2023-03-24 23:22:34 -0600

    cambio de LAMP a WAMP, cambio de paths para un correcto desarrollo

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -4,28 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/build/css/app.css">
+    <link rel="stylesheet" href="/BienesRaicesProject/build/css/app.css">
     <title>Bienes Raices</title>
 </head>
 <body>
     <header class="header <?php echo $inicio ? 'inicio' : '' ?> ">
         <div class="contenedor contenido-header">
             <div class="barra">
-                <a href="/">
-                    <img class="logo" src="/build/img/logo.svg" alt="Logotipo de Bienes Raices">
+                <a href="/BienesRaicesProject/index.php">
+                    <img class="logo" src="/BienesRaicesProject/build/img/logo.svg" alt="Logotipo de Bienes Raices">
                 </a>
 
                 <div class="mobile-menu">
-                    <img src="/build/img/barras.svg" alt="icono menu responsive">
+                    <img src="/BienesRaicesProject/build/img/barras.svg" alt="icono menu responsive">
                 </div>
 
                 <div class="derecha">
-                    <img class="dark-mode-boton"src="/build/img/dark-mode.svg" alt="dark-mode">
+                    <img class="dark-mode-boton"src="/BienesRaicesProject/build/img/dark-mode.svg" alt="dark-mode">
                     <nav class="navegacion">
-                        <a href="/nosotros.php">Nosotros</a>
-                        <a href="/anuncios.php">Anuncios</a>
-                        <a href="/blog.php">Blog</a>
-                        <a href="/contacto.php">Contacto</a>
+                        <a href="/BienesRaicesProject/nosotros.php">Nosotros</a>
+                        <a href="/BienesRaicesProject/anuncios.php">Anuncios</a>
+                        <a href="/BienesRaicesProject/blog.php">Blog</a>
+                        <a href="/BienesRaicesProject/contacto.php">Contacto</a>
                     </nav>
                 </div>
                 
